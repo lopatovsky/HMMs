16cc9a8f484ff7a741489a3e77c4905a1491f306
Lukas Lopatovsky <lopatovsky@gmail.com>
2017-01-27 14:45:09 +0100

    Fixed bug in forward algorithm

--- cthmm/hmm.pyx
+++ cthmm/hmm.pyx
@@ -8,6 +8,7 @@ cimport cython
 #cython: nonecheck=False
 
 ctypedef numpy.float64_t float_t
+ctypedef numpy.int_t int_t
 
 cdef class HMM:
 
@@ -43,12 +44,39 @@ cdef class HMM:
     #    """From given state and emission sequence calculate their likelihood estimation given model parameters"""
     #
 
-    #cpdef emission_estimate(self, emission ):
-    #    """From given emission sequence calculate the likelihood estimation given model parameters"""
-    #    return numpy.sum( self.backward( states,emission)[0,:] )
+    cpdef emission_estimate(self, emission ):
+        """From given emission sequence calculate the likelihood estimation given model parameters"""
+        return numpy.sum(  numpy.exp( self.forward( emission )[-1,:] ) )#TODO underflow
 
-    cpdef numpy.ndarray[float_t, ndim=2] forward(self, numpy.ndarray[float_t, ndim=1] emissions):
+    cpdef numpy.ndarray[float_t, ndim=2] forward(self, numpy.ndarray[int_t, ndim=1] emissions):
         """From emission sequence calculate the forward variables (alpha) given model parameters.
+           Return logaritmus of probabilities.
+        """
+        cdef numpy.ndarray[float_t, ndim=2] loga = self._loga
+        cdef numpy.ndarray[float_t, ndim=2] logb = self._logb
+        cdef numpy.ndarray[float_t, ndim=1] logpi = self._logpi
+        cdef int i, s, size, states_num,
+        cdef float_t max_p, log_sum
+
+        size = emissions.shape[0]
+        states_num = self._a.shape[0]
+        cdef numpy.ndarray[float_t, ndim=2] alpha = numpy.full( (size,states_num), 0, dtype=numpy.float64 ) #numpy.zeros( (size, states_num ))
+
+        alpha[0,:] = logpi + logb[:, int(emissions[0]) ]
+        for i in range(1,size):
+            for s in range(states_num):
+
+                max_p = numpy.amax(  alpha[i-1,:] )                                                      #log-sum-exp trick
+                log_sum = numpy.log ( numpy.sum( numpy.exp( alpha[i-1,:] + loga[:,s] - max_p ) ) ) #
+                alpha[i,s] = max_p + log_sum
+
+            print(  numpy.exp(alpha[i,:]) )
+            alpha[i,:] = alpha[i,:] + logb[:, int(emissions[i]) ]
+
+        return alpha
+
+    cpdef numpy.ndarray[float_t, ndim=2] backward(self, numpy.ndarray[int_t, ndim=1] emissions):
+        """From emission sequence calculate the backward variables beta) given model parameters.
            Return logaritmus of probabilities.
         """
         cdef numpy.ndarray[float_t, ndim=2] loga = self._loga
@@ -58,18 +86,18 @@ cdef class HMM:
 
         size = emissions.shape[0]
         states_num = self._a.shape[0]
-        cdef numpy.ndarray[float_t, ndim=2] alpha = numpy.full( (size,states_num), 0, dtype=numpy.float64 ) #numpy.zeros( (size, states_num ))
-        print(emissions[0])
-        alpha[0,:] = logpi + logb[:, int(emissions[0]) ]
-        for i in range(1,size):
+        cdef numpy.ndarray[float_t, ndim=2] beta = numpy.full( (size,states_num), 0, dtype=numpy.float64 ) #numpy.zeros( (size, states_num ))
+
+        beta[-1,:] = 0  #log(1) = 0
+        for i in range(size-2, -1,-1):
             for s in range(states_num):
-                max_p = numpy.amax(  alpha[i-1,:] )                                                      #log-sum-exp trick
-                log_sum = numpy.log ( numpy.sum( numpy.exp( alpha[i-1,:] + loga[:,s] - max_p ) ) ) #
-                alpha[i,s] = max_p + log_sum
+                max_p = numpy.amax(  beta[i+1,:] )                                                      #log-sum-exp trick
+                log_sum = numpy.log ( numpy.sum( numpy.exp( beta[i+1,:] + loga[s,:] - max_p ) ) ) #
+                beta[i,s] = max_p + log_sum
 
-            alpha[i,:] = alpha[i,:] + logb[:, int(emissions[i]) ]
+            beta[i,:] = beta[i,:] + logb[:, int(emissions[i+1]) ]
 
-        return alpha
+        return beta
 
     cpdef viterbi(self, emissions):
         """From the emission sequence calculate most probable corresponding state sequence given parameters"""
@@ -94,11 +122,14 @@ def main():
     B = numpy.array([[0.9,0.1],[0.2,0.8]])
     pi = numpy.array( [0.8,0.2] )
     hmm = HMM(A,B,pi)
-    (s,e) = hmm.generate(100000)
+    (s,e) = hmm.generate(3)
     print(s)
     print(e)
+    e = numpy.array([0,1])
 
-    print( (hmm.forward(e) ) )
+    print( numpy.exp(hmm.forward(e) ) )
+    print( numpy.exp(hmm.backward(e) ) )
+    print( (hmm.emission_estimate(e) ) )
 
     #hmm2 = HMM.from_parameters(A,B,pi)
     #hmm2 = HMM.from_file("x.hmm")
